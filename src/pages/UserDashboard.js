ea8d129f99ff74b0dee05fc65ee5f40ee80dd391
GauravKesh <gkrcoder@gmail.com>
2024-07-08 23:01:16 +0530

    d

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -407,7 +407,7 @@ const UserDashboard = () => {
                     />
                     <dialog id="my_modal_5" className="modal">
                       <div className="modal-box">
-                        <form method="dialog">
+                        <form method="dialog" className="fixed top-2 right-2">
                           <button className="absolute  btn btn-sm btn-circle z-50 btn-ghost  right-2 top-2 bg-slate-200 hover:bg-slate-600">
                             ✕
                           </button>
